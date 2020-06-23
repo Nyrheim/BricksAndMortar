4e9621ca8b85f981788f6620f906f6febe2e4e98
LiamVII <44938368+LiamVII@users.noreply.github.com>
2020-06-23 06:22:40 +0100

    Allow staff to set player profession, closes #24

--- src/main/java/net/nyrheim/bricksandmortar/command/profession/ProfessionSetCommand.java
+++ src/main/java/net/nyrheim/bricksandmortar/command/profession/ProfessionSetCommand.java
@@ -28,24 +28,37 @@ public final class ProfessionSetCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        Player target = null;
+        String professionName;
         if (!(sender instanceof Player)) {
             sender.sendMessage(RED + "You must be a player to perform this command.");
             return true;
+        } else {
+            target = (Player) sender;
         }
         if (args.length < 1) {
             sender.sendMessage(RED + "Usage: /" + label + " set [profession]");
             return true;
         }
-        Player player = (Player) sender;
+        professionName = args[0];
+        if (args.length > 1) {
+            if (sender.hasPermission("bricksandmortar.command.profession.set.other")) {
+                target = plugin.getServer().getPlayer(args[0]);
+                if (target == null) {
+                    sender.sendMessage(RED + "There is no player online by that name.");
+                    return true;
+                }
+                professionName = args[1];
+            }
+        }
         PenPlayerService playerService = plugin.getServices().get(PenPlayerService.class);
-        PenPlayer penPlayer = playerService.getPlayer(player);
+        PenPlayer penPlayer = playerService.getPlayer(target);
         PenCharacterService characterService = plugin.getServices().get(PenCharacterService.class);
         PenCharacter character = characterService.getActiveCharacter(penPlayer);
         if (character == null) {
             sender.sendMessage(RED + "You do not currently have an active character.");
             return true;
         }
-        String professionName = args[0];
         BricksProfessionService professionService = plugin.getServices().get(BricksProfessionService.class);
         Profession profession = professionService.getProfession(professionName);
         if (profession == null) {
@@ -53,18 +66,24 @@ public final class ProfessionSetCommand implements CommandExecutor {
             return true;
         }
         if (professionService.getProfession(character) != null) {
-            sender.sendMessage(RED + "You already have a profession.");
-            return true;
+            if (target == sender) {
+                sender.sendMessage(RED + "You already have a profession.");
+                return true;
+            }
         }
         professionService.setProfession(character, profession);
-        player.getInventory().addItem(
+        Player finalTarget = target;
+        finalTarget.getInventory().addItem(
                 profession.getToolTypes().stream()
                         .map(toolType -> new PenItemStack(toolType, 1).toItemStack())
                         .toArray(ItemStack[]::new)
         )
                 .values()
-                .forEach(item -> player.getWorld().dropItem(player.getLocation(), item));
-        sender.sendMessage(GREEN + "Profession set to " + profession.getName() + ".");
+                .forEach(item -> finalTarget.getWorld().dropItem(finalTarget.getLocation(), item));
+        if (sender != target) {
+            sender.sendMessage(GREEN + "" + target.getName() + "'s Profession set to " + profession.getName() + ".");
+        }
+        target.sendMessage(GREEN + "Profession set to " + profession.getName() + ".");
         return true;
     }
 }
