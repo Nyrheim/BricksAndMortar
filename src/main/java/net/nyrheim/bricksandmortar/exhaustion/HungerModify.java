a50dcd200574bb186ff346351fd9e154e719f41d
LiamVII <44938368+LiamVII@users.noreply.github.com>
2020-06-23 09:24:14 +0100

    Added crafter hunger mechanics, closes #38

--- src/main/java/net/nyrheim/bricksandmortar/exhaustion/HungerModify.java
+++ src/main/java/net/nyrheim/bricksandmortar/exhaustion/HungerModify.java
@@ -21,4 +21,18 @@ public class HungerModify {
         }
     }
 
+    public static void updateHunger(Player player, int itemExh) {
+        int foodLevel = player.getFoodLevel();
+        double exhMod = Math.round(itemExh / 5.0);
+        if (foodLevel > 15) {
+            player.setFoodLevel(foodLevel - (6 + (int) exhMod));
+        }
+        else if (foodLevel < 15 && foodLevel > 10) {
+            player.setFoodLevel(foodLevel - (4 + (int) exhMod));
+        }
+        else if (foodLevel <= 10) {
+            player.setFoodLevel(foodLevel - (2 + (int) exhMod));
+        }
+    }
+
 }
