d5a8fa42cc94be958afcbb224c925b8d272be0ff
Ren Binden <ren.binden@gmail.com>
2020-06-28 23:21:09 +0100

    Add exhaustion values to alchemy items, closes #43

--- src/main/java/net/nyrheim/bricksandmortar/item/InteractItemLookup.java
+++ src/main/java/net/nyrheim/bricksandmortar/item/InteractItemLookup.java
@@ -2,19 +2,22 @@ package net.nyrheim.bricksandmortar.item;
 
 import org.bukkit.inventory.ItemStack;
 
-public class InteractItemLookup {
+public final class InteractItemLookup {
+
+    private InteractItemLookup() {}
 
     public static boolean isItemInteractSafe(ItemStack itemStack) {
         switch (itemStack.getType()) {
-            case BREAD: return true;
-            case APPLE: return true;
-            case COOKED_BEEF: return true;
-            case COOKED_CHICKEN: return true;
-            case COOKED_COD: return true;
-            case COOKED_MUTTON: return true;
-            case COOKED_PORKCHOP: return true;
-            case COOKED_RABBIT: return true;
-            case COOKED_SALMON: return true;
+            case BREAD:
+            case APPLE:
+            case COOKED_BEEF:
+            case COOKED_CHICKEN:
+            case COOKED_COD:
+            case COOKED_MUTTON:
+            case COOKED_PORKCHOP:
+            case COOKED_RABBIT:
+            case COOKED_SALMON:
+                return true;
             default: return false;
         }
     }
